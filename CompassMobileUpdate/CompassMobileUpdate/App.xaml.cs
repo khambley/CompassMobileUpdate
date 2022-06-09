9ea694834ee3b9aac6c4c23c2943c98a96b0e860
Katherine Hambley <C971939@exelonds.com>
2022-06-09 09:06:57 -0500

    Added LoginPage and NavPage

--- CompassMobileUpdate/CompassMobileUpdate/App.xaml.cs
+++ CompassMobileUpdate/CompassMobileUpdate/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CompassMobileUpdate.Pages;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace CompassMobileUpdate
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
